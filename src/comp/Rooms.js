c68c5ef685f846f4d43bac5776aef4a03a611c27
camillecamahort <camillecamahort@gmail.com>
2018-02-20 16:16:20 -0800

    HQ game

--- src/comp/Rooms.js
+++ src/comp/Rooms.js
@@ -11,6 +11,9 @@ class Rooms extends Component {
             <div>
                 <button onClick = {this.props.handleDisplay.bind(this,"room1")}>Room1</button>
                 <button onClick = {this.props.handleDisplay.bind(this,"room2")}>Room2</button>
+                <button onClick = {this.props.handleDisplay.bind(this,"room3")}>Room3</button>
+                <button onClick = {this.props.handleDisplay.bind(this,"room4")}>Room4</button>
+                <button onClick = {this.props.handleDisplay.bind(this,"room5")}>Room5</button>
             </div>
         )
     }
